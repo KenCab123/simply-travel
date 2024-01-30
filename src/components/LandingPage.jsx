25b42338a664421b10f0bb14b469d6a4fde83e4a
Jennifer <jenniferpeterson@pursuit.org>
2024-01-30 15:47:35 -0500

    set up header component

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,5 +1,11 @@
 const LandingPage = () => {
-  return <div></div>;
+  return (
+    <header>
+      <h2>Explore. Relax. Enjoy.</h2>
+      <h1>Welcome to Simply Travel</h1>
+      <button>Your journey awaits</button>
+    </header>
+  );
 };
 
 export default LandingPage;
