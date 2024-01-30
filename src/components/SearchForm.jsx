39681fbfd54932ea6e2f8f68a3693e315ea0d990
Jennifer <jenniferpeterson@pursuit.org>
2024-01-30 16:00:15 -0500

    Search Form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -10,6 +10,21 @@ const SearchForm = () => {
           <option value="cold">cold</option>
         </select>
       </label>
+      <label htmlFor="departure">
+        Departure Airport
+        <input id="departure" name="departure" type="text" />
+      </label>
+
+      <label htmlFor="start-date">
+        Departure Date
+        <input id="start-date" name="start-date" type="date" />
+      </label>
+
+      <label htmlFor="end-date">
+        Return Date
+        <input id="end-date" name="end-date" type="date" />
+      </label>
+      <button type="submit">🔍</button>
     </form>
   );
 };
