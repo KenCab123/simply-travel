8ab14fce4293568646827319f0165a2c7d64f0d7
KenCab123 <KennethCabral@pursuit.org>
2024-01-31 10:55:46 -0500

    Implemented a way to grab the users IP, edited a route to the IndexPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import React from "react";
 import NavBar from "./components/NavBar";
 import LandingPage from "./components/LandingPage";
 import { Routes, Route } from "react-router-dom";
 import AboutUs from "./components/AboutUs";
-import SearchForm from "./components/SearchForm";
+import IndexPage from "./components/IndexPage";
 import Cart from "./components/Cart";
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const App = () => {
+  const [ip, setIP] = useState("");
+
+  const getData = async () => {
+    const res = await axios.get("https://api.ipify.org/?format=json");
+    console.log(res.data);
+    setIP(res.data.ip);
+  };
+
+  useEffect(() => {
+    //passing getData method to the lifecycle method
+    getData();
+  }, []);
+
+  
   return (
     <>
       <NavBar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="/destinations" element={<SearchForm />} />
+        <Route path="/destinations" element={<IndexPage/>} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </>
