1ca2bf8dcac4fb0444470efc63b6f2e968211939
Iris Beckham <irisdancer3@gmail.com>
2024-02-01 13:23:57 -0800

    find flight prices based on object of cheapest flights from api request

--- src/components/DestinationPage.jsx
+++ src/components/DestinationPage.jsx
@@ -6,25 +6,38 @@ import { Link, useLocation } from "react-router-dom";
 const DestinationPage = ({ setDestinations, destinations }) => {
   let location = useLocation()
 
-  const [cheapestFlight, setCheapestFlight] = useState({})
+  const [cheapestFlights, setCheapestFlights] = useState({})
+  const URL = location.state.URL;
 
-  let URL = location.state.URL
   useEffect(() => {
     getDestinations().then(data => {
       setDestinations(data)
     })
-    getCheapestFlight(URL)
+    getCheapestFlight(URL).then(data => {
+      setCheapestFlights(data.data)
+    })
   }, [])
 
+  const findFirstObjectKey = (obj, key) => {
+    const objectAtKey = obj[key]
+    if (!objectAtKey) {
+      return "No flights available"
+      //maybe fetch????
+    }
+    const firstKey = Object.keys(objectAtKey)[0]
+    return obj[key][firstKey].price
+  }
+
   return <div>
-    {destinations.map(destination => {
-      URL += `&destination=${destination.iata}`
-      // setDestinationState(destination)
-      // console.log(URL)
-      // console.log(getCheapestFlight(URL))
+    {Object.keys(cheapestFlights).length > 0 && destinations.map(destination => {
       return (
         <Link to={`/${destination.id}`} key={destination.id} >
-          <li >{destination.destination} {destination.iata}</li>
+          <li >
+            {destination.destination} {destination.iata}
+            <p>Price: {
+              findFirstObjectKey(cheapestFlights, destination.iata)
+            }</p>
+          </li>
         </Link>
       )
     })}
