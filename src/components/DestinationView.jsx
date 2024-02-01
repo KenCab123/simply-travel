8c8b37268311e157c52d58588af9dc1ccc02f9ae
Iris Beckham <irisdancer3@gmail.com>
2024-02-01 09:35:28 -0800

    lifted the destinations state to app.jsx

--- src/components/DestinationView.jsx
+++ src/components/DestinationView.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useLocation, useParams } from "react-router-dom";
 
-export const DestinationView = () => {
+const DestinationView = ({ destinations }) => {
+  const { id } = useParams();
+  console.log(id)
+  console.log(destinations)
+  const destination = destinations.find((destination) => destination.id === id);
+  console.log(destination)
+  // const location = useLocation();
+  // console.log(location.state.destination)
+  // console.log(location.state)
+  // console.log(state.destinationState)
   return (
     <div>DestinationView</div>
   )
 }
+
+export default DestinationView;
